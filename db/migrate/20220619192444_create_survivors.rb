b81a9485f703af9a989fca6d48123e0a4fa70e90
Stéfany Larissa <stelar.dev@gmail.com>
2022-06-19 16:33:54 -0300

    survivors migrate and name field corrected

--- db/migrate/20220619192444_create_survivors.rb
+++ db/migrate/20220619192444_create_survivors.rb
@@ -1,7 +1,7 @@
 class CreateSurvivors < ActiveRecord::Migration[7.0]
   def change
     create_table :survivors do |t|
-      t.string :age
+      t.string :name
       t.integer :age
       t.string :gender
       t.string :last_location
